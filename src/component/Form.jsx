ee6dae451c6c07a72956f586ceadf5fa1b94f6b0
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-09-30 23:01:36 -0500

    desktop version

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,12 +1,15 @@
 const Form = () =>{
   const global_premiun = 'w-[100%] min-h-[13.20vh] rounded-[10px] bg-purple-100 px-[17.87vw]'
+  const global_premiun_xl = 'xl:w-[37.5vw] xl:px-0 xl:min-h-[7.5vh]'
   const composition_premiun = 'text-[15px] leading-[174%] tracking-[0.27px]'
   const utilities_text = 'text-white text-center'
   const utilities_flex = 'flex justify-center items-center'
   const block_drop_shadow = 'shadow-premiun-shadow'
 
-  const global_form = 'w-[87.2vw] bg-white rounded-[10px] p-[6.4vw]'
-  const global_input = 'border border-grey-100 w-[100%] min-h-[56px] rounded-[5px] px-[5.07vw]'
+  const global_form = 'w-[100%] bg-white rounded-[10px] p-[6.4vw]'
+  const global_form_xl = 'xl:w-[37.5vw] p-[2.78vw]'
+  const global_input = 'border border-grey-100 w-[100%] min-h-[56px] rounded-[5px] px-[5.07vw] text-[14px] font-semibold tracking-[0.25px] text-grey-200'
+  const global_input_xl = 'xl:px-[2.22vw]'
   const global_placeholder = 'placeholder-grey-200 placeholder:text-[14px] placeholder:tracking-[0.25px] placeholder:font-semibold placeholder:opacity-70'
   const global_button = 'w-[100%] min-h-[56px] bg-green-100 rounded-[5px] uppercase text-[15px] font-semibold text-white tracking-[0.9px]'
   const block_drop_shadow_button = 'shadow-button-shadow'
@@ -14,22 +17,22 @@ const Form = () =>{
   return(
     <>
       <article className="flow__form">
-        <div className={`premiun ${global_premiun} ${composition_premiun} ${utilities_text} ${utilities_flex} ${block_drop_shadow}`}>
+        <div className={`premiun ${global_premiun} ${composition_premiun} ${utilities_text} ${utilities_flex} ${block_drop_shadow} ${global_premiun_xl}`}>
           <p>
             <span className={`font-bold`}>Try it free 7 days</span> then $20/mo. thereafter
           </p>
         </div>
-        <form className={`${global_form} ${block_drop_shadow}`} id="myForm">
+        <form className={`form ${global_form} ${block_drop_shadow} ${global_form_xl}`} id="myForm">
           <fieldset >
             <legend className="sr-only">Personal Information</legend>
               <label className="sr-only" htmlFor="firstName">First Name</label>
-              <input className={`${global_input} ${global_placeholder}`} type="text" id="firstName" name="firstName" placeholder="First Name" autoComplete="name"/>
+              <input className={`${global_input} ${global_placeholder} ${global_input_xl}`} type="text" id="firstName" name="firstName" placeholder="First Name" autoComplete="name"/>
               <label className="sr-only" htmlFor="lastName">Last Name</label>
-              <input className={`${global_input} ${global_placeholder}`} type="text" id="lastName" name="lastName" placeholder="Last Name" autoComplete="name" data-type="top"/>
+              <input className={`${global_input} ${global_placeholder} ${global_input_xl}`} type="text" id="lastName" name="lastName" placeholder="Last Name" autoComplete="name" data-type="top"/>
               <label className="sr-only" htmlFor="email">Email Address</label>
-              <input className={`${global_input} ${global_placeholder}`} type="email" id="email" name="email" placeholder="Email Address" autoComplete="email" data-type="top"/>
+              <input className={`${global_input} ${global_placeholder} ${global_input_xl}`} type="email" id="email" name="email" placeholder="Email Address" autoComplete="email" data-type="top"/>
               <label className="sr-only" htmlFor="password">Password</label>
-              <input className={`${global_input} ${global_placeholder}`} type="password" id="password" name="password" placeholder="Password" autoComplete="password" data-type="top"/>
+              <input className={`${global_input} ${global_placeholder} ${global_input_xl}`} type="password" id="password" name="password" placeholder="Password" autoComplete="password" data-type="top"/>
               <button className={`${global_button} ${block_drop_shadow_button}`} type="submit" data-type="top">
                 Claim your free trial
               </button>
--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -1,14 +1,17 @@
 const Title = () =>{
   const composition_h1 = 'text-[28px] font-bold -trancking-[0.29px] leading-[128.57%]'
+  const composition_h1_xl = 'xl:text-[50px] xl:leading-[110%] xl:tracking-[-0.52px]'
   const composition_p = 'font-medium leading-[162.5%]'
   const utilities_text = 'text-white text-center'
+  const utilities_text_xl = 'xl:text-left'
+  const composition_container_xl = 'xl:w-[36.46vw] xl:mr-[3.125vw]'
   return(
     <>
-      <article className="flow__title">
-        <h1 className={`title ${composition_h1} ${utilities_text}`}>
+      <article className={`container flow__title ${composition_container_xl}`}>
+        <h1 className={`title ${composition_h1} ${utilities_text} ${composition_h1_xl} ${utilities_text_xl}`}>
           Learn to code by watching others
         </h1>
-        <p className={`description ${composition_p} ${utilities_text}`}>
+        <p className={`description ${composition_p} ${utilities_text} ${utilities_text_xl}`}>
           See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
           but understanding how developers think is invaluable.
         </p>
